534c110b0e8f35f3809d8b0bf88c2d2eadb52f95
PaperBrr <120294723+paperbrr@users.noreply.github.com>
2024-02-29 00:27:36 +0530

    continued init for sprite-phys

--- src/framework/Sprite/sprite.h
+++ src/framework/Sprite/sprite.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SDL.h>
+#include "../Block/block.h"
 
 //sprite struct declaration
 typedef struct{
@@ -21,3 +22,4 @@ Sprite* createSprite(SDL_Color spriteColor);
 void renderSprite(Sprite* sprite, SDL_Renderer* renderer);
 void freeSprite(Sprite* sprite);
 void spriteMoveUpdate(Sprite* sprite, SDL_Event* event);
+int spriteColliding(Sprite* sprite, Block* head);
