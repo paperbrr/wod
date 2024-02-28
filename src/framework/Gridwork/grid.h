a4f1833f43f6e680ff94e5b05c1e61d040645326
PaperBrr <120294723+paperbrr@users.noreply.github.com>
2024-02-29 01:59:07 +0530

    initial upload

---
+++ src/framework/Gridwork/grid.h
@@ -0,0 +1,54 @@
+#pragma once
+
+
+typedef struct Cell{
+    
+    //filled or not :D, 1 is true, 0 is false
+    int filled;
+
+    //cell pos, measured from top left
+    int cellX;
+    int cellY;
+
+}Cell;
+
+
+typedef struct {
+
+    //cell list
+    Cell** cellArr;
+
+    //metadata
+    int winH;
+    int winW;
+    int cellCount;
+    int cellSize;
+
+}Grid;
+
+
+typedef enum{
+
+    //options for finding cells, given a certain cell
+    CELL_UP,
+    CELL_DOWN,
+    CELL_LEFT,
+    CELL_RIGHT
+
+}LookupOption;
+
+
+typedef enum{
+
+    //to fill or not to fill; that is the question
+    UNFILL, //0
+    FILL,   //lmao, 1
+
+}FillState;
+
+
+//function declarations
+Grid* createGrid(int winW, int winH, int cellSize);
+Cell* cellFindFromPos(Grid* grid, int x, int y);
+Cell* cellFindNeighbour(Grid* grid, Cell* cell, LookupOption dir);
+void cellChangeInArea(Grid* grid, int x, int y, int w, int h, FillState fillState);
