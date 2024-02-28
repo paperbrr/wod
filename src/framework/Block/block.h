8e2ba99e1b4fb8163a5e2166b670b3217d4b2f28
PaperBrr <120294723+paperbrr@users.noreply.github.com>
2024-02-28 17:07:02 +0530

    declared block type and block struct, related functions

---
+++ src/framework/Block/block.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <SDL.h>
+
+
+//enum to deal with different types of blocks
+typedef enum{
+
+    BLOCK_NORMAL,
+    BLOCK_ACTION
+
+}BlockType;
+
+
+typedef struct Block{
+
+    //body
+    SDL_Rect* sourceRect;
+    SDL_Color blockColor;
+
+    //offset positions (of center), have to match with top left 
+    float xMid;
+    float yMid;
+
+    //type and action (with void* arg, just typecast)
+    BlockType blockType;
+    void (*actionFunc) (void*);
+
+    //links to other blocks
+    struct Block* previousNode;
+    struct Block* nextNode;
+
+}Block;
+
+Block* createBlock(Block* head, SDL_Color blockColor, int width, int height);
+void freeBlock(Block* block);
+void renderBlockNode(Block* head, SDL_Renderer* renderer);
