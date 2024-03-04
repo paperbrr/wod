d5574f2f739f842b77c77a5cce944424e324ab3a
PaperBrr <120294723+paperbrr@users.noreply.github.com>
2024-03-04 14:51:51 +0530

    change test conditions (WRITE TESTS PLEASE)

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <SDL.h>
 #include <stdio.h>
+#include <math.h>
 
 #include "framework/UIElements/UI.h"
 #include "framework/Sprite/sprite.h"
@@ -17,19 +18,18 @@ int main(int argc, char* argv[]) {
 
     SDL_Init(SDL_INIT_VIDEO);
     
-    window = SDL_CreateWindow("BOOTLEG PAINT", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
+    window = SDL_CreateWindow("BOOTLEG PAINT", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 810, 600, 0);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    Grid* grid = createGrid(810, 600, 30);
 
 
     SDL_Color newColor = {135, 206, 250, 255};
     
     
     SDL_Color groundCol = {255, 230, 153, 255};
-    Block* ground = createBlock(NULL, groundCol, 800, 100, BLOCK_NORMAL);
-    ground->sourceRect->x = 0; ground->sourceRect->y = 500;
-
-    Block* obstacle = createBlock(ground, groundCol, 50, 50, BLOCK_NORMAL);
-    obstacle->sourceRect->x = 200; obstacle->sourceRect->y = 100;
+    Block* ground = createBlock(NULL, groundCol, 810, 90, 0, 510, BLOCK_NORMAL, grid);
+    //Block* obstacle = createBlock(ground, groundCol, 50, 50, 200, 100, BLOCK_NORMAL, grid);
 
     int gameLoop = 1;
     SDL_Event event;
@@ -53,6 +53,14 @@ int main(int argc, char* argv[]) {
             }
             if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP){
                 spriteMoveUpdate(newSprite, &event);
+            }
+            if (event.type == SDL_MOUSEBUTTONDOWN){
+                int xPos = event.motion.x; int yPos = event.motion.y;
+                xPos = 30*floor((double) xPos/30);
+                yPos = 30*floor((double) yPos/30);
+                Cell* yes = cellFindFromPos(grid, xPos, yPos);
+                if (yes) {printf("%i\n",yes->filled);}
+                else {printf("tomfoolery :D");}
             }
         }
         SDL_Delay(10);
